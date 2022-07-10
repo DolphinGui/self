7d37d1e933a3ef6fb7b5f7a02769211cbb0af3a1
DolphinGui <umeda.shin@gmail.com>
2022-07-09 19:08:04 -0700

    I have found out I need to provide _start. Fun

--- tests/integration_test.cpp
+++ tests/integration_test.cpp
@@ -2,6 +2,7 @@
 #include "lexer.hpp"
 #include "syntax_tree.hpp"
 
+#include <cstdlib>
 #include <fmt/core.h>
 #include <iostream>
 #include <llvm/Support/raw_ostream.h>
@@ -11,6 +12,8 @@
 constexpr auto file = "\
 \
 fun main()->int{return 1;}";
+constexpr auto path = "a.o";
+constexpr auto output_name = "a.out";
 
 int main() {
   auto AST = selflang::lex(file);
@@ -18,10 +21,12 @@ int main() {
   auto &IR = AST.codegen();
   IR.print(llvm::outs(), nullptr);
   std::error_code file_err;
-  auto aout = llvm::raw_fd_ostream("a.o", file_err);
+  auto aout = llvm::raw_fd_ostream(path, file_err);
   if (file_err) {
     llvm::errs() << "Could not open file: " << file_err.message();
     return 1;
   }
   selflang::compile(IR, aout);
+  auto command = fmt::format("ld {} -o{}", path, output_name);
+  std::system(command.c_str());
 }
